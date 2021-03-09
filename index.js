6335a4b1afd593f40331dc2c2198cd1c1865042d
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-03-09 15:26:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function introduction(name) {
     return `Hi, my name is ${name}.`
 }
 
-// try breaking up the long line below
-
 function introductionWithLanguage(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    return `Hi, my name is ${name} and I am learning to program `
+     + `in ${language}.`
 }
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    return `Hi, my name is ${name} and I am learning to program `
+     + `in ${language}.`
 }
